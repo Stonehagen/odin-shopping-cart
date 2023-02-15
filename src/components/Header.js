978333008b559b87556d811b52731b9de470eb06
Stonehagen <steinhagen@mailbox.org>
2023-02-16 00:04:31 +0100

    add styling for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HeaderImg from '../images/header/header.png';
+import '../styles/Header.css';
 
 function Header() {
   return (
-    <div className="Header">
-      <h1>This is the Header</h1>
+    <div
+      className="Header"
+      style={{
+        backgroundImage: `url(${HeaderImg})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}
+    >
       <ul>
-        <Link to="/"><li>HOME</li></Link>
-        <Link to="/shop"><li>SHOP</li></Link>
+        <Link to="/">
+          <li>HOME</li>
+        </Link>
+        <Link to="/shop">
+          <li>SHOP</li>
+        </Link>
       </ul>
     </div>
   );
