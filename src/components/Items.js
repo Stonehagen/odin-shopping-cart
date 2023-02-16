d1bf3c885dbc04847bfd3a8460fe3dc7363e83f1
Stonehagen <steinhagen@mailbox.org>
2023-02-16 20:52:22 +0100

    add sails to itemList for shop

---
+++ src/components/Items.js
@@ -0,0 +1,58 @@
+import Duke from '../images/sails/Duke.webp';
+import Idol from '../images/sails/IDOL.webp';
+import SuperStarSLS from '../images/sails/SUPERSTAR_SLS.webp'
+import SuperStarHD from '../images/sails/SUPERSTAR_HD.webp'
+import SuperHero from '../images/sails/SUPERHERO.webp'
+
+export const ShopItems = [
+  {
+    catagory: 'Sails',
+    name: 'DUKE',
+    brand: 'Duotone',
+    picture: Duke,
+    description:
+      "THE ULTIMATE JUMP N' RIDE SAIL BEING AN IDEAL MATCH WITH FREEWAVE AND SMALL FREERIDE BOARDS",
+    size: '5.0qm',
+    price: '919€',
+  },
+  {
+    catagory: 'Sails',
+    name: 'IDOL LTD',
+    brand: 'Duotone',
+    picture: Idol,
+    description:
+      "THE LIGHTEST YET MOST COMPETITIVE FREESTYLE SAIL WE HAVE EVER BUILT!",
+    size: '5.2qm',
+    price: '1019€',
+  },
+  {
+    catagory: 'Sails',
+    name: 'SUPER_STAR SLS',
+    brand: 'Duotone',
+    picture: SuperStarSLS,
+    description:
+      'AS LIGHT AS A MEMBRANE SAIL, BUT WITHOUT SACRIFICING DURABILITY/LONGEVITY. WE ARE SO CONFIDENT OF THE BRAND-NEW SLS CONSTRUCTION THAT WE BACK IT UP WITH OUR UNIQUE 5-YEAR WARRANTY. SLS - STRONG LIGHT SUPERIOR!',
+    size: '4.5qm',
+    price: '1209€',
+  },
+  {
+    catagory: 'Sails',
+    name: 'SUPER_STAR HD',
+    brand: 'Duotone',
+    picture: SuperStarHD,
+    description:
+      'ONE OF THE MOST ICONIC WAVE SAILS EVER! RESPONSIVE AND DIRECT THE NEW SUPER_STAR HD WORKS ON SINGLE- AND MULTIFIN WAVE BOARDS AND EVEN FOR FREEWAVE JUMPING CONDITIONS.',
+    size: '4.7qm',
+    price: '1009€',
+  },
+  {
+    catagory: 'Sails',
+    name: 'SUPER_HERO',
+    brand: 'Duotone',
+    picture: SuperHero,
+    description:
+      'LOOK NO FURTHER - THE WAVE SAIL FOR ANY CONDITION AND RIDER WEIGHT. THE CENTERED DRAFT POSITION MAKES THE SUPER_HERO THE MOST NEUTRAL WAVE SAIL OUT THERE.',
+    size: '4.0qm',
+    price: '979€',
+  },
+];
