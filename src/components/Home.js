0a8f1a3c8066358ac4787e31b76516a8812654eb
Stonehagen <steinhagen@mailbox.org>
2023-02-17 23:20:06 +0100

    add some lorem ipsum to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,32 @@ const Home = () => {
   return (
     <div className="Home">
       <h1>Wellcome to WNDSRFNG.</h1>
-      <p>Here you will find the finest Windsurfing Equipment, Pro Gossip and Event News.</p>
+      <p>
+        Here you will find the finest Windsurfing Equipment, Pro Gossip and
+        Event News.
+      </p>
+      <h3>
+        Cillum exercitation ad dolor nisi dolore ad pariatur irure ullamco esse.
+      </h3>
+      <p>
+        Deserunt adipisicing labore ad proident Lorem exercitation in ex ex.
+        Minim do id aliquip non dolor. Laboris magna sit do anim nulla elit. Ea
+        tempor exercitation exercitation incididunt mollit esse sint amet
+        incididunt do ad. Non minim irure mollit occaecat laboris aliquip ex id
+        adipisicing. Cupidatat eu sint ut elit consequat.
+      </p>
+      <h3>
+        Cillum exercitation ad dolor nisi dolore ad pariatur irure ullamco esse.
+      </h3>
+      <p>
+        Deserunt adipisicing labore ad proident Lorem exercitation in ex ex.
+        Minim do id aliquip non dolor. Laboris magna sit do anim nulla elit. Ea
+        tempor exercitation exercitation incididunt mollit esse sint amet
+        incididunt do ad. Non minim irure mollit occaecat laboris aliquip ex id
+        adipisicing. Cupidatat eu sint ut elit consequat.
+      </p>
     </div>
   );
-}
+};
 
 export default Home;
