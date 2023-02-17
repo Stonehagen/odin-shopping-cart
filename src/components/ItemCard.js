3fc885c81f3b86644f6ecdd38b6cc27489387c37
Stonehagen <steinhagen@mailbox.org>
2023-02-17 13:09:09 +0100

    change addToCart function

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,20 +2,32 @@ import React from 'react';
 import '../styles/ItemCard.css';
 
 const ItemCard = ({ item, cart, setCart }) => {
-
   const addToCart = (it) => {
-    setCart(cart.concat([it]));
-  }
+    const edibleCart = [...cart];
+    let found = false;
+    edibleCart.forEach((cartItem, index) => {
+      if (edibleCart[index].name === it.name) {
+        found = true;
+        edibleCart[index].amount = edibleCart[index].amount + 1;
+      }
+    });
+    if (!found) {
+      const newEntry = { name: it.name, amount: 1, item: it };
+      edibleCart.push(newEntry);
+    }
+    setCart(edibleCart);
+  };
+
   return (
     <div className="Item-Card">
-      <div className='Item-Image-Wrapper'>
+      <div className="Item-Image-Wrapper">
         <img src={item.picture} alt={item.name} />
       </div>
       <h3>
         {item.name}, {item.size}
       </h3>
       <h4>{item.brand}</h4>
-      <div className='Price-Wrapper'>
+      <div className="Price-Wrapper">
         <h4>{item.price}€</h4>
         <button onClick={() => addToCart(item)}>Add to Cart</button>
       </div>
